9fefa65dcae4b32296b3c5f6bab9bad05026e25c
Nina Marra <snipnina@gmail.com>
2017-03-20 22:40:40 -0300

    Installing crud generator

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         * External service providers
         */
         Spatie\Permission\PermissionServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -230,6 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
 
     ],
 
